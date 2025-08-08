d76607ec581319d15915e409ea9a2269d5626b89
cvillar6 <camilovilla699@gmail.com>
2025-08-07 22:05:52 -0500

    ♻️ Se agregan ambientes

---
+++ frontend/src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    apiURL: 'http://localhost:1337',
+    apiToken: 'b81ca6c81c5fb3bc16148db37d356a869f6dc5c840184a59f96800c143eacbda6f2beb51c0c646bb2af7e8fb1322d16a2449c8ba2e423ab2ef4c76c4671586fe76ef5da4e91aed74646430df1fe2f8955e820c5dfef921fb53c1a032611c33cabeeb0f6064899e8887d90d7ca13482777c16a65be6313a5573a73ecd5647c846'
+};
